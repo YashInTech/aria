9b15c0333dacfb5ddec55006dd4e3fb647ae58e5
YashInTech <YashInTech@users.noreply.github.com>
2025-01-10 21:50:10 +0530

    Added Middlewares

--- backend/src/routes/user-router.ts
+++ backend/src/routes/user-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
+import { loginValidator, signupValidator, validate } from "../utils/Validators.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
-userRouter.post("/signup", userSignup);
+userRouter.post("/signup", validate(signupValidator), userSignup);
+userRouter.post("/login", validate(loginValidator), userLogin);
  
 export default userRouter;
