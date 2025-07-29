c8d7b69cadff0cd5fe1c91cfdfc6ffd192c8aee2
YashInTech <YashInTech@users.noreply.github.com>
2025-07-30 02:03:00 +0530

    updated the route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,10 @@
-import { Router } from "express";
-import userRouter from "./user-router.js";
-import chatRouter from "./chat-router.js";
+import { Router } from 'express';
+import userRouter from './user-router.js';
+import chatRouter from './chat-router.js';
 
 const appRouter = Router();
 
-appRouter.use("/user", userRouter); //domain/api/v1/user
-appRouter.use("/chats", chatRouter); //domain/api/v1/chats
-
+appRouter.use('/user', userRouter); //domain/api/v1/user
+appRouter.use('/chat', chatRouter); //domain/api/v1/chats
 
 export default appRouter;
